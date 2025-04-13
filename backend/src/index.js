4cbdef3af8c4f93e48e3629087a7273be2af89ad
X19 ♾️ <yourbuddypushpal@gmail.com>
2025-04-13 23:26:51 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000")
+app.use("/api/auth", authRoutes)
+
+app.listen(5001, () => {
+    console.log("Server is running on port 5001")
 })
